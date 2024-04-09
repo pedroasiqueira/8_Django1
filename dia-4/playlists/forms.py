54c865f5ce39a3bda2c6384dee7dcb27fc409fcf
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2024-04-09 18:23:05 -0300

    dia 4

--- dia-4/playlists/forms.py
+++ dia-4/playlists/forms.py
@@ -1,7 +1,7 @@
 # playlists/forms.py
 from django import forms
 from playlists.validators import validate_music_length, validate_name_singer
-from playlists.models import Music
+from playlists.models import Music, Singer
 
 
 class CreateMusicForm(forms.Form):
@@ -35,13 +35,19 @@ class CreateMusicModelForm(forms.ModelForm):
     class Meta:
         model = Music
         fields = "__all__"
-        labels = {
-            "name": "Nome da música",
-            "recorded_at": "Data de gravação",
-            "length_in_seconds": "Duração em segundos",
-        }
-        widgets = {
-            "recorded_at": forms.DateInput(
-                attrs={"type": "date", "value": "2023-07-06"}
-            )
-        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["name"].label = "Nome da música"
+        self.fields["recorded_at"].label = "Data de gravação"
+        self.fields["recorded_at"].widget = forms.DateInput(
+                attrs={"type": "date"})
+        self.fields["recorded_at"].initial = "2023-07-06"
+        self.fields["length_in_seconds"].label = "Duração em segundos"
+        self.fields["singer"].label = "Artista"
+        self.fields["singer"].widget = forms.Select(
+            choices=[
+                (singer.id, singer.name)
+                for singer in Singer.objects.filter(name__contains="e")
+            ]
+        )
