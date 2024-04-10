6221bbfbfe89f70fc5929c8cee759407f7c59684
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2024-04-09 21:53:09 -0300

    dia 5

---
+++ dia-5/django-rest-framework/playlistify/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from core.views import SingerViewSet, MusicViewSet, PlaylistViewSet
+
+router = routers.DefaultRouter()
+router.register(r'singers', SingerViewSet)
+router.register(r'musics', MusicViewSet)
+router.register(r'playlists', PlaylistViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+]
